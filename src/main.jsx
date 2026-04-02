6669cfd8ecd851dc48baed0be3de0aa7b97209f5
ruthadi7 <Gashawruth@gmail.com>
2026-04-02 23:04:42 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./app/App.jsx";
-import "./globals.css";   // ✅ correct path
+import "./globals.css";   
 
 ReactDOM.createRoot(document.getElementById("root")).render(
    <div className="WholeBackgroundColor">
